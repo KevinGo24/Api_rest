4f5834263165a7e44b0b35b524e582f0e0678a5c
kevin <kevin@gmail.com>
2026-05-06 12:38:08 -0500

    agregar

---
+++ Scripts/API.js
@@ -0,0 +1,5 @@
+// llamo a las dependecia de express
+const Exp = require('express');
+const App = express();
+// la variable va utilizar toda las librerias de exprees en formato json
+App.use(express.json())
